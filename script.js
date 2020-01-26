785170eaba91feaf37a2e36a71b945e3e9f3a2d4
Kate <tarakanovskayak@gmail.com>
2020-01-27 01:25:16 +1000

    Validate phone and email field

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log('loh pidr');
 function validateForm() {
     validateName();
     validateMessage();
+    validateEmailOrPhoneIsNotEmpty();
 }
 
 function validateName() {
@@ -16,11 +17,34 @@ function validateMessage() {
 }
 
 function validateRequiredField(input, idErrorMessageContainer) {
+    input.classList.remove('invalid');
+    let errorContainer = document.getElementById(idErrorMessageContainer);
+    errorContainer.innerHTML = '';
     if (input.validity.valueMissing) {
-        let classList = input.classList;
-        classList.add('invalid');
-        let errorContainer = document.getElementById(idErrorMessageContainer);
-        errorContainer.innerHTML = 'Поле обязательно для заполнения';
+        input.classList.add('invalid');
+        errorContainer.innerHTML = 'Поле обязательно для заполнения.';
+    }
+}
+
+function validateEmailOrPhoneIsNotEmpty() {
+    let email = document.getElementsByName("email")[0];
+    let phone = document.getElementsByName("phone")[0];
+
+    email.classList.remove('invalid');
+    phone.classList.remove('invalid');
+
+    let emailErrorContainer = document.getElementById('email-error');
+    let errorContainer = document.getElementById('phone-error');
+
+    emailErrorContainer.innerHTML = '';
+    errorContainer.innerHTML = '';
+
+    if (!email.value && !phone.value) {
+        email.classList.add('invalid');
+        phone.classList.add('invalid');
+
+        emailErrorContainer.innerHTML = 'Поле Эл.почта или Телефон должны быть заполнены.';
+        errorContainer.innerHTML = 'Поле Эл.почта или Телефон должны быть заполнены.';
     }
 }
 
@@ -31,6 +55,8 @@ window.onload = function() {
 
     sendButton.addEventListener('click', () => {
         validateForm();
+
+        let messageTextarea = document.getElementsByName("email")[0];
     });
 
 }
